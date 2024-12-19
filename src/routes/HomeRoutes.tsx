dd0f4be956743b3357a113f39faf74722bcedaa2
tinhtq <truongquangtinh1997@gmail.com>
2024-12-19 23:11:14 +0700

    fix issue

--- src/routes/HomeRoutes.tsx
+++ src/routes/HomeRoutes.tsx
@@ -1,12 +1,12 @@
 import React, { lazy, Suspense } from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { PATH } from "../constants/paths";
 const Home = lazy(() => import("../pages/Home/Home"));
 
 export default function HomeRoutes() {
   return (
     <Routes>
-      <exact path={PATH.HOME} component={() => <Home />} />
+      <Route path={PATH.HOME} element={<Home />} />
     </Routes>
   );
 }
